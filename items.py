a44539a9a1d74225962682eeb40f1ad4daaf8a20
kriskraw <chriscrawley80@gmail.com>
2022-05-27 15:25:06 +0000

    1125

--- items.py
+++ items.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 items = [
-    {'num': 1, 'name': 'light bulb', 'price': 2.49},
-    {'num': 2, 'name': 'laundry soap', 'price': 3.98},
-    {'num': 3, 'name': 'phone charger', 'price': 2.49}
+    {'num': '1', 'name': 'light bulb', 'price': 2.49},
+    {'num': '2', 'name': 'laundry soap', 'price': 3.98},
+    {'num': '3', 'name': 'phone charger', 'price': 12.49}
 ]
--- store.py
+++ store.py
@@ -5,54 +5,84 @@ from items import items
 cart = []
 shop = True
 
-print('welcome to Amazon :)')
+print('\nwelcome to Amazon :)')
+
+def getItem(itemNum):
+    for item in items:
+        if itemNum == item['num']:
+            return item
+        
 
 def listItems():
+    print('item# \t name  \t\t price')
+    print('==============================')
     for item in items:
         itemNum = item['num']
         itemName = item['name']
         itemPrice = item['price']
         print(str(itemNum) + ' \t ' + itemName + ' \t ' + str(itemPrice))
 
+def addItem(menuChoice):
+    global cart
+    itemNum = menuChoice[2::]
+    cart.append(itemNum)
+    itemName = getItem(itemNum)['name']
+    print(f'{itemName} has been added to your cart...')
+
+def showCart():
+    global cart
+    print('your cart contains: ')
+    print('item# \t name  \t price')
+    print('==============================')
+    subtotal = 0
+    for item in cart:
+        itemName = getItem(item)['name']
+        itemPrice = getItem(item)['price']
+        subtotal += itemPrice
+        print(itemName + ' \t ' + str(itemPrice))
+    print(f'\nsubtotal: $ {subtotal}')
+
+
+def clearCart():
+    global cart
+    cart = []
+    print('your cart has been cleared...')
+
+
+def checkOut():
+    print('your order has been placed....')
+    exitApp()
+
+
 def exitApp():
     shop = False
     print('thank you for shopping with Amazon. see you soon :)')
     quit()
 
+
 def showMenu():
-    print('\n\ni  list items')
+    print('\ni  list items')
     print('a itemnum  add item to cart')
     print('s  show shopping cart')
     print('c  clear shopping cart')
-    print('b  buy items')
+    print('o  check out')
     print('e  exit')
 
+
 def main():
-    global cart
     while shop == True:
         showMenu()
         menuChoice = input('? ').lower()
-        print('*' + menuChoice[0:2:1] + '*')
         if menuChoice == "i":
             listItems()
         elif menuChoice[0:2:1] == 'a ':
-            itemNum = menuChoice[2::]
-            print(itemNum)
-            #if itemNum in items:
-            cart.append(itemNum)
-            print(cart)
-            print('    has been added to your cart...')
+            addItem(menuChoice)
         elif menuChoice == "s":
-            print('your cart contains: ')
-            print(cart)
+            showCart()
         elif menuChoice == "c":
-            print(cart)
-            cart = []
-            print(cart)
-            print('your cart has been cleared...')
-        elif menuChoice == "b":
-            print('your order has been placed....')
-            exitApp()
+            clearCart()
+        elif menuChoice == "o":
+            checkOut()
         elif menuChoice == "e":
             exitApp()
 
